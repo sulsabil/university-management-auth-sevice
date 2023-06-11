1b008dbe58c9fac3cfafd2c4cf4379cd99629c78
SABILA <salsabilnishat@gmail.com>
2023-06-11 20:17:49 +0600

    [done] setup express mpngoose cors parser

---
+++ src/config/index.ts
@@ -0,0 +1,9 @@
+import dotenv from 'dotenv';
+import  Path from 'path';
+
+dotenv.config({path: Path.join(process.cwd(), '.env')})
+
+export default{
+    port: process.env.PORT,
+    database_url: process.env.DATABASE_URL
+}
---
+++ src/server.ts
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import app from "./app";
+import config from "./config/index";
+
+async function databaseConnecting() {
+    try {
+        await mongoose.connect(config.database_url as string);
+        console.log('database is connected');
+
+        app.listen(config.port, () => {
+            console.log(`Application listening on port ${config.port}`)
+        })
+    } catch (error) {
+        console.log('failed to connected', error);
+    }
+}
+
+databaseConnecting(); 

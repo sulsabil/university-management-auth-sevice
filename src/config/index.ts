e94c095f2fe481ece12b1e7c7cfe0ca61be9bac8
SABILA <salsabilnishat@gmail.com>
2023-06-11 23:11:51 +0600

    testing

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,9 +1,9 @@
-import dotenv from 'dotenv';
-import  Path from 'path';
+import dotenv from 'dotenv'
+import Path from 'path'
 
-dotenv.config({path: Path.join(process.cwd(), '.env')})
+dotenv.config({ path: Path.join(process.cwd(), '.env') })
 
-export default{
-    port: process.env.PORT,
-    database_url: process.env.DATABASE_URL
+export default {
+  port: process.env.PORT,
+  database_url: process.env.DATABASE_URL,
 }
